482cadc5e66ff04e46419c2138f4bce56572ccb7
Allister Aligerum <allister@aligerum.io>
2020-06-30 00:08:23 -0400

    add vue components

---
+++ template/vueComponent.js
@@ -0,0 +1,9 @@
+module.exports = {
+  script: 'componentName.vue',
+  publicData: [],
+  // publicData: async data => {
+  // },
+  tags: [
+    // {tag: 'script', attributes: {src: "//somesite.com/somescript.js", async: true}},
+  ],
+}
---
+++ template/vueComponent.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+.componentName
+</template>
+
+<style lang="stylus" scoped>
+// .componentName
+</style>
+
+<script>
+export default {
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  mounted() {
+  },
+  methods: {
+  },
+  props: {
+  },
+  watch: {
+  },
+}
+</script>
