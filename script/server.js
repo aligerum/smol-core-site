4b0ddc71102aad93148bfdd78bbfaa2ee2ddd718
Allister Aligerum <allister@aligerum.io>
2020-07-01 03:37:18 -0400

    add message on server startup

--- script/server.js
+++ script/server.js
@@ -10,4 +10,4 @@ app.use(express.static(`${process.cwd()}/output/${smol.coreName}/public`, {exten
 app.disable('x-powered-by')
 
 // listen on configured port
-app.listen(coreConfig.port)
+app.listen(coreConfig.port, () => console.log(smol.colors.green(`${smolConfig.appName} Site core (${smol.coreName}) listening on port ${coreConfig.port})))
