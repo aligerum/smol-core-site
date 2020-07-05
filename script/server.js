cf63a8eb61c76d1420e0c93ff5879f7af4c1ee97
Allister Aligerum <allister@aligerum.io>
2020-07-05 00:25:26 -0400

    fix syntax error in server script

--- script/server.js
+++ script/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const smol = require('smol')
+const smolConfig = smol.config()
 const coreConfig = smol.config(smol.coreName)
 
 // serve from output/<coreName>/public
@@ -10,4 +11,4 @@ app.use(express.static(`${process.cwd()}/output/${smol.coreName}/public`, {exten
 app.disable('x-powered-by')
 
 // listen on configured port
-app.listen(coreConfig.port, () => console.log(smol.colors.green(`${smolConfig.appName} Site core (${smol.coreName}) listening on port ${coreConfig.port})))
+app.listen(coreConfig.port, () => console.log(smol.colors.green(`${smolConfig.appName} Site core (${smol.coreName}) listening on port ${coreConfig.port}`)))
